2d8793643301a001509df4ed26e7c8d06dea6d69
htmlclassic <htmlclassic@gmail.com>
2023-11-20 17:05:30 +0300

    refactor: delete redundant import

--- src/app/sell/page.tsx
+++ src/app/sell/page.tsx
@@ -1,7 +1,6 @@
 import { getAPI } from "@/supabase/api";
 import { createOtherSupabaseClient } from "@/supabase/utils_server";
 import Table from "./Table";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
