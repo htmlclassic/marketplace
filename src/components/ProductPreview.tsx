208b2719b8c87e05daffb28769a709afb1b5c624
htmlclassic <htmlclassic@gmail.com>
2023-11-20 14:42:34 +0300

    fix(product_preview): make responsive

--- src/components/ProductPreview.tsx
+++ src/components/ProductPreview.tsx
@@ -7,17 +7,9 @@ interface ProductProps {
 }
 
 export default function ProductPreview({ product }: ProductProps) {
-  const outOfStock = product.quantity === 0;
-
   return (
-    <div className="flex flex-col gap-3 relative">
-      {
-        outOfStock &&
-        <div className="absolute rounded-lg bg-black bg-opacity-60 backdrop-blur-[2px] w-full h-full z-10 text-white flex justify-center items-center select-none">Out of stock</div>
-      }
-      <Link href={`/products/${product.id}`}>
-        <div className="group/parent max-h-[380px] overflow-hidden space-y-2">
-          <div className="relative h-[260px]">
+      <Link href={`/products/${product.id}`} className="group/parent overflow-hidden space-y-2">
+          <div className="relative aspect-square">
             <Image
               src={product.imageUrls ? product.imageUrls[0] : ImgPlaceholder}
               fill
@@ -27,8 +19,6 @@ export default function ProductPreview({ product }: ProductProps) {
           </div>
           <div className="line-clamp-1 font-bold transition group-hover/parent:text-sky-500">{product.price} ₽</div>
           <div className="line-clamp-2 break-words transition group-hover/parent:text-sky-500">{product.title}</div>
-        </div>
       </Link>
-    </div>
   );
 }
