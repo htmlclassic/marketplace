57d5395a3907d06bde7d8b6483b33adb626477f6
htmlclassic <htmlclassic@gmail.com>
2023-11-20 17:04:54 +0300

    fix(nextconfig): fix bodySizeLimit option

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 
 const nextConfig = {
   experimental: {
-    serverActionsBodySizeLimit: '4.5mb' // 4.5mb is vercel's limit for uploading files, idk how to overcome it yet
+    serverActions: {
+      bodySizeLimit: '4.5mb'
+    }
   },
 
   images: {
