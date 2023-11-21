9e5e4075e16a5e33cd1a7a42890db0fa5a0b34b6
htmlclassic <htmlclassic@gmail.com>
2023-11-21 12:10:28 +0300

    feat(/search): user can search by category

--- src/app/search/ProductList.tsx
+++ src/app/search/ProductList.tsx
@@ -10,15 +10,26 @@ interface Props {
 
 export default function ProductList({ products }: Props) {
   const searchParams = useSearchParams();
-  const targetText = searchParams.get('text')!.toLocaleLowerCase();
+  const text = searchParams.get('text');
+  const category = searchParams.get('category');
 
-  let list: React.ReactNode = <p>К сожалению мы не нашли совпадений :(</p>
+  let list: React.ReactNode = <p className="w-max">К сожалению мы не нашли совпадений :(</p>
 
   if (products) {
-    const filteredProducts = products.filter(product => 
-      product.quantity &&
-      product.title.toLowerCase().includes(targetText)
-    );
+    const filteredProducts = products.filter(product => {
+      if (product.quantity < 1) return false;
+
+      if (text && category) {        
+        return (
+          product.title.toLowerCase().includes(text.toLocaleLowerCase()) &&
+          product.category.toLowerCase() === category.toLocaleLowerCase()
+        );
+      } else if (text) {
+        return product.title.toLowerCase().includes(text.toLocaleLowerCase());
+      } else if (category) {
+        return product.category.toLowerCase() === category.toLocaleLowerCase();
+      }
+    });
 
     if (filteredProducts.length) {
       list = filteredProducts.map(pr => <ProductPreview key={pr.id} product={pr} />)
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,10 +4,12 @@ import ProductList from './ProductList';
 
 export default async function Catalog() {
   const api = getAPI(createServerComponentSupabaseClient());
+  // no need to select all the products
+  // later implement WHERE in getProducts
   const products = await api.getProducts();
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,260px)] justify-center gap-7 w-full">
+    <div className="grid content-start grid-cols-2 justify-center min-[560px]:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-7 w-full">
       <ProductList  products={products} />
     </div>
   );
