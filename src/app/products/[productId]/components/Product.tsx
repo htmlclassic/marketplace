85eea2f9dfa540e03c7ac6664eb598a67283c8c2
htmlclassic <htmlclassic@gmail.com>
2023-11-21 12:09:02 +0300

    feat: category in a product is a link now

--- src/app/products/[productId]/components/Product.tsx
+++ src/app/products/[productId]/components/Product.tsx
@@ -1,6 +1,7 @@
 import Slider from '@/src/components/Slider';
 import Image from 'next/image';
 import ImgPlaceholder from '@/src/components/noimage.jpg'
+import Link from 'next/link';
 
 interface ProductProps {
   product: Product;
@@ -38,7 +39,15 @@ export default function Product({ product, children }: ProductProps) {
       {children}
       <div className="flex flex-col gap-5">
         <p className="text-lg font-bold text-sky-600">{product.price} ₽</p>
-        <p className="flex gap-5 items-center">Категория: <span className="border  rounded-3xl p-2">{product.category}</span></p>
+        <p className="flex gap-5 items-center">
+          Категория:
+          <Link
+            href={`/search?category=${product.category}`}
+            className="border rounded-3xl p-2 transition-all hover:border-slate-400"
+          >
+            {product.category}
+          </Link>
+        </p>
         <p>Количество товара: {product.quantity}</p>
         <p className="flex flex-col gap-3">
           <span className="font-bold">Описание</span>
